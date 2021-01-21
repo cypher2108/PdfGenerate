f2b2728d3a72c1788b2fc024f4f438250c12fc77
Praful Sharma <praful.sharma_cs18@gla.ac.in>
2021-01-20 19:59:51 -0800

    understood the adding of custom font

--- src/App.java
+++ src/App.java
@@ -1,12 +1,27 @@
 import java.util.Scanner;
 
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Text;
 
 public class App {
+    
+    public static final String georgia = "./fonts/Georgia.ttf";
+    public static final String georgiaItalic = "./fonts/GeorgiaItalic.ttf";
+    public static final String georgiaBold = "./fonts/GeorgiaBold.ttf";
+    public static final String georgiaBoldItalic = "./fonts/GeorgiaBoldItalic.ttf";
+
     public static void main(String[] args) throws Exception {
+
+        PdfFont geo = PdfFontFactory.createFont(georgia, true);
+        PdfFont geoBold = PdfFontFactory.createFont(georgiaBold, true);
+        PdfFont geoItalic = PdfFontFactory.createFont(georgiaItalic, true);
+        PdfFont geoBoldItalic = PdfFontFactory.createFont(georgiaBoldItalic, true);
+
         System.out.println("Hello, World!");
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a valid file name: ");
@@ -20,10 +35,24 @@ public class App {
 
         String text = "Hello World!";
         Paragraph paragraph = new Paragraph(text);
-        
+
+        Text text1 = new Text("Sample Text 1").setFont(geo);
+        Text text2 = new Text("Sample Text 2").setFont(geoBold);
+        Text text3 = new Text("Sample Text 3").setFont(geoItalic);
+        Text text4 = new Text("Sample Text 4").setFont(geoBoldItalic);
+
+        Paragraph paragraph2 = new Paragraph()
+        .add(text1)
+        .add(text2)
+        .add(text3)
+        .add(text4);
+
         Document document = new Document(pdfDocument);
         document.add(paragraph);
+        document.add(paragraph2);
         document.close();
+
+        
        
         scanner.close();
 
