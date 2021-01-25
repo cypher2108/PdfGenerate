03f38492590580b094b93d4f6689275deeed9fd9
Praful Sharma <praful.sharma_cs18@gla.ac.in>
2021-01-24 22:22:52 -0800

    Added Projects and Education Section into the code

--- src/main/Resume.java
+++ src/main/Resume.java
@@ -51,7 +51,7 @@ public class Resume {
         Color myColor = new DeviceRgb(89, 89, 89);
         Color socialHandles = new DeviceRgb(29, 130, 76);
         
-// Tile paragraph code Start here ==============================================================================
+        // Tile paragraph code Start here ==================================
         
         Paragraph title = new Paragraph();
         title.setMultipliedLeading(0.8f);
@@ -155,18 +155,153 @@ public class Resume {
 
 
         title.setTextAlignment(TextAlignment.CENTER);
-// Tile paragraph code ended ==============================================================================
+        // Tile paragraph code ended ==============================================================================
 
+        //Objective paragraph started===========================
         Paragraph aboutYourself = new Paragraph();
         aboutYourself.setMultipliedLeading(1.0f);
 
         System.out.println("Tell us about yourself");
         Text aboutMe = new Text(scanner.nextLine().trim());
+        aboutMe.setFont(clibri);
         aboutMe.setFontSize(11);
         aboutMe.setFontColor(myColor);
 
         aboutYourself.add(sampleLine);
         aboutYourself.add(aboutMe);
+        //Objective paragraph ended==============================
+
+        //Experrience or Projects=======================
+
+        Paragraph projects = new Paragraph();
+        projects.setMultipliedLeading(1.0f);
+
+        Text projectsHeading = new Text("PROJECTS");
+        projectsHeading.setFont(geoBold);
+        projectsHeading.setFontSize(14);
+
+        System.out.println("Project Heading 1");
+        Text proHead1 = new Text(scanner.nextLine().trim());
+        proHead1.setFont(clibriBold);    
+        proHead1.setFontSize(13);
+        proHead1.setFontColor(socialHandles);
+
+        System.out.println("Project 1 Description");
+        Text proDescription1 = new Text(scanner.nextLine().trim());
+        proDescription1.setFont(clibriBold);    
+        proDescription1.setFontSize(11);
+        proDescription1.setFontColor(myColor);
+
+        System.out.println("Project Heading 2");
+        Text proHead2 = new Text(scanner.nextLine().trim());
+        proHead2.setFont(clibriBold);    
+        proHead2.setFontSize(13);
+        proHead2.setFontColor(socialHandles);
+
+        System.out.println("Project 2 Description");
+        Text proDescription2 = new Text(scanner.nextLine().trim());
+        proDescription2.setFont(clibriBold);    
+        proDescription2.setFontSize(11);
+        proDescription2.setFontColor(myColor);
+
+        System.out.println("Project Heading 3");
+        Text proHead3 = new Text(scanner.nextLine().trim());
+        proHead3.setFont(clibriBold);    
+        proHead3.setFontSize(13);
+        proHead3.setFontColor(socialHandles);
+
+        System.out.println("Project 3 Description");
+        Text proDescription3 = new Text(scanner.nextLine().trim());
+        proDescription3.setFont(clibriBold);    
+        proDescription3.setFontSize(11);
+        proDescription3.setFontColor(myColor);
+
+
+        
+
+        projects.add(sampleLine);
+        projects.add(projectsHeading);
+        projects.add(sampleLine);
+        projects.add(sampleLine);
+        projects.add(proHead1);
+        projects.add(sampleLine);
+        projects.add(proDescription1);
+
+        projects.add(sampleLine);
+        projects.add(sampleLine);
+        projects.add(proHead2);
+        projects.add(sampleLine);
+        projects.add(proDescription2);
+
+        projects.add(sampleLine);
+        projects.add(sampleLine);
+        projects.add(proHead3);
+        projects.add(sampleLine);
+        projects.add(proDescription3);
+
+        //Education=======================
+        Paragraph education = new Paragraph();
+        education.setMultipliedLeading(1.0f);
+
+        Text educationHeading = new Text("EDUCATION");
+        educationHeading.setFont(geoBold);
+        educationHeading.setFontSize(14);
+
+        System.out.println("Enter your School or college in Monocromatically Decreasing order");
+        Text eduHead1 = new Text(scanner.nextLine().trim());
+        eduHead1.setFont(clibriBold);    
+        eduHead1.setFontSize(13);
+        eduHead1.setFontColor(socialHandles);
+
+        System.out.println("Dates? do it like moth, year - month year");
+        Text date1 = new Text(scanner.nextLine().trim());
+        date1.setFont(clibri);    
+        date1.setFontSize(11);
+        date1.setFontColor(myColor);
+
+        System.out.println("Education 1 Description");
+        Text eduDescription1 = new Text(scanner.nextLine().trim());
+        eduDescription1.setFont(clibriBold);    
+        eduDescription1.setFontSize(11);
+        eduDescription1.setFontColor(myColor);
+
+        System.out.println("Enter your School or college");
+        Text eduHead2 = new Text(scanner.nextLine().trim());
+        eduHead2.setFont(clibriBold);    
+        eduHead2.setFontSize(13);
+        eduHead2.setFontColor(socialHandles);
+
+        System.out.println("Dates?");
+        Text date2 = new Text(scanner.nextLine().trim());
+        date2.setFont(clibri);    
+        date2.setFontSize(11);
+        date2.setFontColor(myColor);
+
+        System.out.println("Education 2 Description");
+        Text eduDescription2 = new Text(scanner.nextLine().trim());
+        eduDescription2.setFont(clibriBold);    
+        eduDescription2.setFontSize(11);
+        eduDescription2.setFontColor(myColor);
+
+        education.add(sampleLine);
+        education.add(educationHeading);
+        education.add(sampleLine);
+        education.add(sampleLine);
+        education.add(eduHead1);
+        education.add(sampleLine);
+        education.add(date1);
+        education.add(sampleLine);
+        education.add(eduDescription1);
+
+        education.add(sampleLine);
+        education.add(sampleLine);
+        education.add(eduHead2);
+        education.add(sampleLine);
+        education.add(date2);
+        education.add(sampleLine);
+        education.add(eduDescription2);
+
+        
 
         //initializing the pdf document object.
         Document document = new Document(pdfDocument);
@@ -174,6 +309,8 @@ public class Resume {
         //adding paragraph to document.
         document.add(title);
         document.add(aboutYourself);
+        document.add(projects);
+        document.add(education);
         document.close();
         scanner.close();
     }
