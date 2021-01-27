05a3ad2c37ff6cfd9cea49d3b18021524b903ba4
Praful Sharma <praful.sharma_cs18@gla.ac.in>
2021-01-27 09:13:08 +0530

    make changes in library

--- src/main/Resume.java
+++ src/main/Resume.java
@@ -307,11 +307,27 @@ public class Resume {
         Paragraph skills = new Paragraph();
         skills.setMultipliedLeading(1.0f);
 
+        Text skillsHeading = new Text("SKILLS");
+        skillsHeading.setFont(geoBold);
+        skillsHeading.setFontSize(14);
+
         Text bulletPoint = new Text("\u2022");
         bulletPoint.setFont(geoBold);
         bulletPoint.setFontSize(14);
         bulletPoint.setFontColor(socialHandles);
 
+        // float [] pointColoumnWidths = {150f, 150f};
+        Table skillsTable = new Table(2);
+        Cell cell1 = new Cell();
+        cell1.add(new Paragraph("hello"));
+        skillsTable.addCell(cell1);
+
+        skills.add(sampleLine);
+        skills.add(sampleLine);
+        skills.add(skillsHeading);
+        skills.add(sampleLine);
+        // skills.add(skillsTable);
+        
 
 
         //initializing the pdf document object.
@@ -322,7 +338,7 @@ public class Resume {
         document.add(aboutYourself);
         document.add(projects);
         document.add(education);
-        document.add(skills);
+        document.add(skillsTable);
         document.close();
         scanner.close();
     }
